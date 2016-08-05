3cf993e35888a60685804d0461b68fc10f451d23
fsjoyti <fsjoyti@iastate.edu>
2016-08-04 17:20:06 -0700

    Played around with debugging the app

--- app/src/main/java/com/example/anastasia/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/anastasia/funfacts/FunFactsActivity.java
@@ -1,13 +1,14 @@
 package com.example.anastasia.funfacts;
 
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,6 +25,7 @@ import org.json.JSONObject;
 
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private Button mShowbutton;
     private TextView mFactTextView;
     private RelativeLayout mRelativeLayout;
@@ -78,5 +80,7 @@ public class FunFactsActivity extends AppCompatActivity {
                 queue.add(stringRequest);
             }
         });
+        //Toast.makeText(FunFactsActivity.this,"Welcome to Cat Facts!!",Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"On onCreate method");
     }
 }
